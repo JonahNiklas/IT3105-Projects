feat: trying to fix large gradient problem
- water level too quickly decreasing

consys.py
@@ -23,48 +23,42 @@ def run_one_timestep(params, plant, controller: Controller, target):
     error = jnp.abs(target - plant.get_output())
     control_signal = controller.calculate_control_signal(params, error)
     plant.timestep(control_signal)
-    return plant.get_output(), control_signal
+    return error
 
 
 def run_one_epoch(params):
     controller = get_controller()
     plant, target = get_plant()
-    output = []
-    control_signal = []
+    errors = []
     for _ in range(SIMULATION_TIMESTEPS):
-        output_t, control_signal_t = run_one_timestep(params, plant, controller, target)
-        output.append(output_t)
-        control_signal.append(control_signal_t)
+        error = run_one_timestep(params, plant, controller, target)
+        errors.append(error)
 
-    mse = jnp.mean((target - jnp.array(output)) ** 2)
-
+    mse = jnp.mean(jnp.array(errors))
     return mse
-
-
 def get_params():
     if CONTROLLER == "pid":
-        params = np.random.uniform(0, 1, 3)
+        params = np.random.uniform(0, 0.1, 3)
 
     elif CONTROLLER == "neural_net":
-        layers = NEURAL_NETWORK
+        layers = NEURAL_NETWORK["neurons_per_layer"]
         params = []
-        sender = layers[0]
-        for receiver in layers[1:]:
+        sender = 3
+        for receiver in layers:
             weights = np.random.uniform(
-                NEURAL_NETWORK["weightRange"][0],
-                NEURAL_NETWORK["weightRange"][1],
+                NEURAL_NETWORK["weight_range"][0],
+                NEURAL_NETWORK["weight_range"][1],
                 (sender, receiver),
             )
             biases = np.random.uniform(
-                NEURAL_NETWORK["biasRange"][0],
-                NEURAL_NETWORK["biasRange"][1],
+                NEURAL_NETWORK["bias_range"][0],
+                NEURAL_NETWORK["bias_range"][1],
                 (1, receiver),
             )
             sender = receiver
             params.append([weights, biases])
     else:
         raise ValueError("Invalid controller type in config")
-
     return params
 
 
@@ -74,7 +68,7 @@ def get_controller():
 
     elif CONTROLLER == "neural_net":
         controller = NeuralNetController(
-            activation_functions=NEURAL_NETWORK["activationFunctions"]
+            activation_functions=NEURAL_NETWORK["activation_functions"]
         )
     else:
         raise ValueError("Invalid controller type in config")
@@ -97,7 +91,7 @@ def get_plant():
             marginal_cost=COURNOT_COMPETITION["marginal_cost"],
             # q1=INI,
             # q2=config["initialQ2"],
-            # noise_range=config["noise_range"],
+            noise_range=NOISE_RANGE
         )
         target = COURNOT_COMPETITION["target_profit"]
     # elif config["plant"] == "plant3":
@@ -109,18 +103,22 @@ def get_plant():
 
 
 def main():
-    errors = []
-    params = get_params()
-    for i in range(TRAINING_EPOCHS):
-        print(f"Epoch {i}")
-        mse, gradients = jax.value_and_grad(run_one_epoch)(params)
-        errors.append(mse)
-        params = params - gradients * LEARNING_RATE
-
-        # Update contrrol signal
-    plt.plot(errors)
-    plt.show()
+  mse_epochs = []
+  params = get_params()
+  for i in range(TRAINING_EPOCHS):
+    print(f"Epoch {i+1}")
+    print(f"Params: {params}")
+    mse, gradients = jax.value_and_grad(run_one_epoch)(params)
+    mse_item = mse.item()
+    print(f"MSE: {mse_item}")
+    print(f"Gradients: {gradients}")
+    mse_epochs.append(mse_item)
+    params = params - gradients * LEARNING_RATE
+    print("====================================")
+    # Update control signal
+  plt.plot(mse_epochs)
+  plt.show()
 
 
 if __name__ == "__main__":
-    main()
+  main()

controllers.py
@@ -1,3 +1,4 @@
+import jax.numpy as jnp
 class Controller:
 
     def __init__(self):
@@ -11,13 +12,42 @@ class Controller:
 class PIDController(Controller):
 
     def calculate_control_signal(self, parameters, error):
-        kp, kd, ki = parameters
         self.error_history.append(error)
         delta_error = error - self.error_history[-2] if len(self.error_history) > 1 else 0
-        return error * parameters[0] + delta_error * parameters[1] + sum(self.error_history) * parameters[2]
+        input = jnp.array([error, delta_error, sum(self.error_history)])
+
+        return parameters.dot(input)
 
 class NeuralNetController(Controller):
+  
+    def __init__(self, activation_functions):
+        super().__init__()
+        self.activation_functions = activation_functions
 
-    def calculate_control_signal(self, parameters, error, activation_functions):
+    def sigmoid(self, x):
+        return 1 / (1 + jnp.exp(-x))
+      
+    def tanh(self, x):
+        return jnp.tanh(x)
+      
+    def relu(self, x):
+        return jnp.maximum(0, x)
+
+    def calculate_control_signal(self, parameters, error):
         self.error_history.append(error)
-        raise NotImplementedError("Implement this method")
+        self.error_history.append(error)
+        delta_error = error - self.error_history[-2] if len(self.error_history) > 1 else 0
+        
+        activation = jnp.array([error, delta_error, sum(self.error_history)])
+        
+        for activation_function, (weight, bias) in zip(self.activation_functions, parameters):
+            if activation_function == "sigmoid":
+                activation = self.sigmoid(activation.dot(weight) + bias)
+            elif activation_function == "tanh":
+                activation = self.tanh(activation.dot(weight) + bias)
+            elif activation_function == "relu":
+                activation = self.relu(activation.dot(weight) + bias)
+            else:
+                print(activation_function)
+                raise ValueError("Invalid activation function in config")
+        return activation

plants.py
@@ -16,18 +16,19 @@ class BathTubPlant(Plant):
         self.area = area
         self.drain_area = drain_area
         self.noise_range = noise_range
-        self.water_level = water_level
+        self.water_level = water_level #+ np.random.uniform(self.noise_range[0],self.noise_range[1])
 
     def get_output(self):
         # could also use derivative of water level
         return self.water_level
 
     def timestep(self, control_signal):
-        if 2*9.81*self.water_level < 0:
-            raise ValueError("Water level is negative")
-        velocity = jnp.sqrt(2*9.81*self.water_level)
+        if self.water_level < 0:
+            print("Water level is negative, this should not happen")
+        velocity =  jnp.sqrt(2*9.81*self.water_level)
+        
         disturbance = np.random.uniform(self.noise_range[0],self.noise_range[1])
-        self.water_level += control_signal + disturbance -self.drain_area*velocity
+        self.water_level = self.water_level + control_signal + disturbance -self.drain_area*velocity
     
 class CournotPlant(Plant):
     def __init__(self, max_price, marginal_cost, q1,q2, noise_range):