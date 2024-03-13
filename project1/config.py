4c17fad170908f1b9637d122f46bc641b878368d
sondresorbye0111 <sondre@magson.no>
2024-03-13 18:13:09 +0100

    hex

--- project1/config.py
+++ project1/config.py
@@ -1,23 +1,23 @@
-VERBOSE = False
+VERBOSE = True
 
-PLANT = "population" # "bathtub" | "cournot" | "population"
+PLANT = "bathtub" # "bathtub" | "cournot" | "population"
 CONTROLLER = "pid" # "pid" | "neural_net
 PID_PARAM_RANGE = [0, 0.5]
 NEURAL_NETWORK = {
-    "neurons_per_hidden_layer": [3,3],
-    "activation_functions": ["relu","relu"], # "sigmoid" | "tanh" | "relu"|"linear"
+    "neurons_per_hidden_layer": [20, 20, 20],
+    "activation_functions": ["tanh", "tanh", "tanh"], # "sigmoid" | "tanh" | "relu"|"linear"
     "output_activation_function": "linear", 
     "weight_range": [-1, 1],
     "bias_range": [-0.5, 0.5]
 }
 TRAINING_EPOCHS = 100
-SIMULATION_TIMESTEPS = 20
+SIMULATION_TIMESTEPS = 15
 LEARNING_RATE = 0.01
-NOISE_RANGE = [-0.01, 0.01]
+NOISE_RANGE = [-0.02, 0.02]
 
 BATHTUB = {
     "cross_sectional_area": 100,
-    "drain_area": 1,
+    "drain_area": 2,
     "initial_water_height": 100,
     "target_water_height": 100,
 }
