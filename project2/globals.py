58812d479823c93d1e22e7ea0c79361760e90800
sondresorbye0111 <sondre@magson.no>
2024-03-28 22:36:05 +0100

    trying to find the reason processes are being killed

--- project2/globals.py
+++ project2/globals.py
@@ -1,9 +1,9 @@
-EXPERIMENT_NAME = "feedforward1"
+EXPERIMENT_NAME = "small-board-long-training"
 VERBOSE = False
 VISUALIZE = False
-BOARD_SIZE = 13
-NUMBER_OF_EPISODES = 500  # actual games
-NUMBER_OF_SIMULATIONS = 50  # MCTS simulations per move
+BOARD_SIZE = 5
+NUMBER_OF_EPISODES = 30000  # actual games
+NUMBER_OF_SIMULATIONS = 100  # MCTS simulations per move
 
 # ANET
 ANET_LEARNING_RATE = 0.01
