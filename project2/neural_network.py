6dc04ff618171ca49893589c014fb532e9a93ed3
Sondre Sørbye <sondre@magson.no>
2024-03-29 18:45:29 +0000

    finding bug

--- project2/neural_network.py
+++ project2/neural_network.py
@@ -14,6 +14,7 @@ from project2.globals import (
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device {device}")
 
+
 class NeuralNetwork(torch.nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
@@ -36,13 +37,21 @@ class NeuralNetwork(torch.nn.Module):
                     torch.nn.Linear(ANET_NUM_HIDDEN_NODES, ANET_NUM_HIDDEN_NODES),
                 )
             if ANET_ACTIVATION_FUNCTION == "linear":
-                self.linear_stack.add_module("activation_function_{}".format(i), torch.nn.Identity())
+                self.linear_stack.add_module(
+                    "activation_function_{}".format(i), torch.nn.Identity()
+                )
             elif ANET_ACTIVATION_FUNCTION == "sigmoid":
-                self.linear_stack.add_module("activation_function_{}".format(i), torch.nn.Sigmoid())
+                self.linear_stack.add_module(
+                    "activation_function_{}".format(i), torch.nn.Sigmoid()
+                )
             elif ANET_ACTIVATION_FUNCTION == "tanh":
-                self.linear_stack.add_module("activation_function_{}".format(i), torch.nn.Tanh())
+                self.linear_stack.add_module(
+                    "activation_function_{}".format(i), torch.nn.Tanh()
+                )
             elif ANET_ACTIVATION_FUNCTION == "relu":
-                self.linear_stack.add_module("activation_function_{}".format(i), torch.nn.ReLU())
+                self.linear_stack.add_module(
+                    "activation_function_{}".format(i), torch.nn.ReLU()
+                )
             else:
                 raise ValueError("Invalid activation function")
         self.softmax = torch.nn.Softmax(dim=1)
@@ -57,11 +66,11 @@ class NeuralNetwork(torch.nn.Module):
         assert logits.shape == (batch_size, BOARD_SIZE * BOARD_SIZE)
         logits = logits.view(batch_size, BOARD_SIZE, BOARD_SIZE)
         return logits
-    
-    def train_one_batch(self, X, Y):
-        assert len(X) == len(Y)
-        X, Y = torch.tensor(X, dtype=torch.float32).to(device), torch.tensor(Y, dtype=torch.float32).to(device)
-        dataloader = torch.utils.data.DataLoader(list(zip(X, Y)), batch_size=ANET_BATCH_SIZE, shuffle=True)
+
+    def train_one_batch(self, RBUF):
+        dataloader = torch.utils.data.DataLoader(
+            RBUF, batch_size=ANET_BATCH_SIZE, shuffle=True
+        )
         if ANET_OPTIMIZER == "adagrad":
             optimizer = torch.optim.Adagrad(self.parameters(), lr=ANET_LEARNING_RATE)
         elif ANET_OPTIMIZER == "sgd":
@@ -71,15 +80,16 @@ class NeuralNetwork(torch.nn.Module):
         elif ANET_OPTIMIZER == "rmsprop":
             optimizer = torch.optim.RMSprop(self.parameters(), lr=ANET_LEARNING_RATE)
         x, y = next(iter(dataloader))
+        x, y = x.to(device), y.to(device)
         optimizer.zero_grad()
         output = self(x)
         loss = torch.nn.functional.mse_loss(output, y)
         loss.backward()
         optimizer.step()
-    
+
     def save(self, i, path="saved_networks"):
         import os
+
         if not os.path.exists(path):
             os.makedirs(path)
         torch.save(self.state_dict(), f"{path}/{EXPERIMENT_NAME}_anet_{i}.pt")
-
--- project2/rl_system.py
+++ project2/rl_system.py
@@ -20,14 +20,19 @@ if VISUALIZE:
 
 
 def play_game(process_id, ANET, RBUF, RBUF_lock, game_num, game_num_lock):
-    # torch.set_num_threads(1)
+    torch.set_num_threads(1)
     while game_num.value < NUMBER_OF_EPISODES:
         game = HexGame(size=BOARD_SIZE, last_move=None)
         mcts = MCTS(ANET=ANET, game_state=game)
         local_RBUF = []
         while not game.is_terminal():
             new_game, distribution = mcts.search()
-            local_RBUF.append((game, distribution))
+            local_RBUF.append(
+                (
+                    torch.tensor(game.get_nn_input(), dtype=torch.float32),
+                    torch.tensor(distribution, dtype=torch.float32),
+                )
+            )
             game = new_game
             if VISUALIZE:
                 game.visualize_board(ax)
@@ -36,14 +41,13 @@ def play_game(process_id, ANET, RBUF, RBUF_lock, game_num, game_num_lock):
             if game_num.value % (NUMBER_OF_EPISODES // ANET_M) == 0:
                 ANET.save(game_num.value)
         if VISUALIZE:
-            print(f"Process {process_id}\tGame {game_num.value}: Player {game.get_winner()} won the game")
-
+            print(
+                f"Process {process_id}\tGame {game_num.value}: Player {game.get_winner()} won the game"
+            )
         with RBUF_lock:
             RBUF.extend(local_RBUF)
-            X = np.array([game.get_nn_input() for game, _ in RBUF])
-            Y = np.array([distribution for _, distribution in RBUF])
 
-        ANET.train_one_batch(X, Y)
+        ANET.train_one_batch(RBUF)
     print(f"Process {process_id} done!")
 
 
@@ -55,7 +59,7 @@ if __name__ == "__main__":
     RBUF_lock = mp.Lock()
     game_num = mp.Value("i", 0)
     game_num_lock = mp.Lock()
-    num_processes = os.cpu_count() + 2
+    num_processes = os.cpu_count()
     print(f"Using {num_processes} processes for training ANET")
     processes = []
     for i in range(num_processes):
@@ -69,8 +73,6 @@ if __name__ == "__main__":
         while game_num.value < NUMBER_OF_EPISODES:
             pbar.update(game_num.value - last_completed_game)
             last_completed_game = game_num.value
-            if not all(process.is_alive() for process in processes):
-                print(f"Process is dead")
             time.sleep(10)
 
     for p in processes:
