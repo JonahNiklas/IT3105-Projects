b7523e92d12e7a27edd6a3b1845e1ac45658f558
Jonah Wiecek <jonahwiecek@gmail.com>
2024-04-12 13:28:03 +0200

    feat: remove invertion of p1,p2 channels

--- project2/supervised.py
+++ project2/supervised.py
@@ -7,21 +7,19 @@ import matplotlib.pyplot as plt
 
 # Load scoredPositionsFull.npz
 data = np.load("project2/scoredPositionsFull.npz")
-# Get a overview of the data
-print(data.files)  # ['positions', 'scores']
+
 # Get the data
 positions = data["positions"]
 training_examples = positions.shape[0]
 print(f"Number of training examples: {training_examples}")
-positions = positions[:, 0:2, :, :]
+
 p1 = positions[:, 0, :, :]
 p2 = positions[:, 1, :, :]
-# Convert from boolean to int
 p1, p2 = p1.astype(float), p2.astype(float)
-# Invert the board
-p1, p2 = 1 - p1, 1 - p2
-empty = np.ones_like(p1) - p1 - p2
+
+empty = np.ones_like(p1) - np.where(p1 + p2 >= 1, 1, 0)
 positions = np.stack([p1, p2, empty], axis=1)
+
 assert positions.shape == (training_examples, 3, 17, 17)
 scores = data["scores"]
 assert scores.shape == (training_examples, 13, 13)
@@ -30,7 +28,8 @@ assert scores.shape == (training_examples, 13, 13)
 scores = np.exp(scores) / np.exp(scores).sum(axis=(1, 2))[:, None, None]
 assert scores.shape == (training_examples, 13, 13)
 
-assert np.isclose(scores[0].sum(), 1.0), f"Sum of scores is not one: {scores[0].sum()}"
+assert np.isclose(
+    scores[0].sum(), 1.0), f"Sum of scores is not one: {scores[0].sum()}"
 
 # Rotate the boards to make more data
 rotated_positions = np.rot90(positions, k=1, axes=(2, 3))
@@ -41,10 +40,13 @@ scores = np.concatenate([scores, rotated_scores], axis=0)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 net = ConvNetwork(board_size=13).to(device)
 print(net)
+
+
 def accuracy(pred, target):
     pred = pred.view(pred.size(0), -1)
     target = target.view(target.size(0), -1)
     return (pred.argmax(dim=1) == target.argmax(dim=1)).float().mean().item()
+
 
 def train(net: torch.nn.Module):
     optimizer = torch.optim.Adam(net.parameters())
@@ -69,6 +71,8 @@ def train(net: torch.nn.Module):
         print(f"Epoch {epoch} loss: {np.mean(epoch_losses)}")
         print(f"Epoch {epoch} accuracy: {np.mean(epoch_accuracies)}")
 
-net.load_state_dict(torch.load("saved_networks/supervised_1.pt", map_location=device))
+
+net.load_state_dict(torch.load(
+    "saved_networks/supervised_1.pt", map_location=device))
 train(net)
 net.save(0, dir="saved_networks", name="supervised")
